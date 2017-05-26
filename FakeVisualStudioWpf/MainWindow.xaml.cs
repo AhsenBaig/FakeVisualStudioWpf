b0711ce53d0256c8c06f45ceaa8c4994ab19186f
Ahsen Baig <ahsenbaig@gmail.com>
2017-05-26 14:37:36 -0700

    Updated the project to include comments and notes about future enhancements.

--- FakeVisualStudioWpf/MainWindow.xaml.cs
+++ FakeVisualStudioWpf/MainWindow.xaml.cs
@@ -7,6 +7,8 @@ namespace FakeVisualStudioWpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        //This is the original title of the application
+        //TODO: might save this somewhere external to keep the users preference.
         string title = "Visual Studio Enterprise 2017";
         public MainWindow()
         {
@@ -19,12 +21,14 @@ namespace FakeVisualStudioWpf
         {
             if (!string.IsNullOrEmpty(UpdateTitle.Text))
             {
+                //Update the title to whatever is in the textbox UpdateTitle.
                 this.Title = UpdateTitle.Text;
             }
         }
 
         private void Reset_Click(object sender, RoutedEventArgs e)
         {
+            //This resets the title back to the original value.
            this.Title = title;
         }
     }
