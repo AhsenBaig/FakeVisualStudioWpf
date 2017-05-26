f7ca4423639dda93c2e258ac9c2262d50b94c31c
Ahsen Baig <ahsenb@cos.edu>
2017-05-26 14:32:15 -0700

    Add project files.

---
+++ FakeVisualStudioWpf/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace FakeVisualStudioWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string title = "Visual Studio Enterprise 2017";
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Initial TItle
+            this.Title = title;
+        }        
+
+        private void Update_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(UpdateTitle.Text))
+            {
+                this.Title = UpdateTitle.Text;
+            }
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+           this.Title = title;
+        }
+    }
+}
