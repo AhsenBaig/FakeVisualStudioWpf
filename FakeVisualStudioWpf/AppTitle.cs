0ce235e61ffb8c627b8270f0f6d989873eb58ded
Ahsen Baig <ahsenbaig@gmail.com>
2017-05-26 15:29:43 -0700

    Added ability for saving users values to to the settings.

---
+++ FakeVisualStudioWpf/AppTitle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeVisualStudioWpf
+{
+    public static class AppTitle
+    {
+        //This is the original title of the application
+        public static readonly string title = "Visual Studio Enterprise 2017";
+        public static string GetSetTitle()
+        {
+            if (!string.IsNullOrEmpty(Properties.Settings.Default.LastSaveTitleValue))
+            {
+                return Properties.Settings.Default.LastSaveTitleValue;
+            }
+            else
+            {
+                return title;
+            }            
+        }
+
+        public static string GetPreviousValue()
+        {
+            //If the previous title is saved we will return that.
+            if (!string.IsNullOrEmpty(Properties.Settings.Default.LastTitleValue))
+            {
+                return Properties.Settings.Default.LastTitleValue;
+            }
+            //Else use the currently saved title.
+            else if (!string.IsNullOrEmpty(Properties.Settings.Default.LastSaveTitleValue))
+            {
+                return Properties.Settings.Default.LastSaveTitleValue;
+            }
+            else
+            {
+                //All else failed return the default
+                return title;
+            }
+
+        }
+    }
+}
--- FakeVisualStudioWpf/MainWindow.xaml.cs
+++ FakeVisualStudioWpf/MainWindow.xaml.cs
@@ -7,29 +7,70 @@ namespace FakeVisualStudioWpf
     /// </summary>
     public partial class MainWindow : Window
     {
-        //This is the original title of the application
-        //TODO: might save this somewhere external to keep the users preference.
-        string title = "Visual Studio Enterprise 2017";
         public MainWindow()
         {
             InitializeComponent();
+
             //Initial TItle
-            this.Title = title;
-        }        
+
+            //Check to see if user has set a title before else use default.
+            this.Title = AppTitle.GetSetTitle();
+
+            //Set the current textbox to be the initial title
+            this.UpdateTitle.Text = this.Title;
+
+            //Update the current title as a backup.
+            UpdateLastTitleUserSettings(this.Title);
+        }
 
         private void Update_Click(object sender, RoutedEventArgs e)
         {
+            //Validate there is value in the text box
             if (!string.IsNullOrEmpty(UpdateTitle.Text))
             {
                 //Update the title to whatever is in the textbox UpdateTitle.
                 this.Title = UpdateTitle.Text;
+
+                //Save the title settings
+                UpdateTitleUserSettings();
             }
         }
 
         private void Reset_Click(object sender, RoutedEventArgs e)
         {
-            //This resets the title back to the original value.
-           this.Title = title;
+            //This resets the title back to the users original value or use current else use the default.
+            this.Title = AppTitle.GetPreviousValue();
+
+            //Save title settings
+            UpdateTitleUserSettings();
+
+            //Update the main textbox to be the title text.
+            UpdateTitle.Text = this.Title;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            //Update and save the title when closing the application.
+            UpdateTitleUserSettings();
+        }
+
+        private void UpdateTitleUserSettings()
+        {
+            //When the window is closing we set the current UpdateTitle to the settings properties.
+            Properties.Settings.Default.LastSaveTitleValue = UpdateTitle.Text;
+
+            //Save the settings
+            Properties.Settings.Default.Save();
+        }
+
+
+        private void UpdateLastTitleUserSettings(string title)
+        {
+            //When the window is closing we set the current UpdateTitle to the settings properties.
+            Properties.Settings.Default.LastTitleValue = title;
+
+            //Save the settings
+            Properties.Settings.Default.Save();            
         }
     }
 }
